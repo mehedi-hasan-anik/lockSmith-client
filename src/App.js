278b3449e99220999986b4801bf3a2ca32489ce9
anik <anikm2283@gmail.com>
2022-06-17 22:32:16 +0600

    Added new side bar

--- src/App.js
+++ src/App.js
@@ -1,85 +1,81 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Admin from './components/Admin/Admin';
-import Projects from './components/Projects/Projects';
-import Login from './components/Login/Login';
-import { createContext, useState } from 'react';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import Contact from './components/Contact/Contact';
-import NotFound from './components/NotFound/NotFound';
-import OrderList from './components/OrderList/OrderList';
-import Order from './components/Order/Order';
+import { createContext, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Admin from "./components/Admin/Admin";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
+import Order from "./components/Order/Order";
+import OrderList from "./components/OrderList/OrderList";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Projects from "./components/Projects/Projects";
 
 export const userContext = createContext();
 
 function App() {
-  const [user,setUser]=useState({
-    isSignIn:false,
-    name:'',
-    email:'',
-    photo:'',
- })
- const [servicePhoto,setServicePhoto]=useState([]);
- const [paymentError,setPaymentError]=useState(null)
- const [paymentSuccess,setPaymentSuccess]=useState(null)
- 
+  const [user, setUser] = useState({
+    isSignIn: false,
+    name: "",
+    email: "",
+    photo: "",
+  });
+  const [servicePhoto, setServicePhoto] = useState([]);
+  const [paymentError, setPaymentError] = useState(null);
+  const [paymentSuccess, setPaymentSuccess] = useState(null);
 
   return (
-    <userContext.Provider value={[user,setUser,servicePhoto,setServicePhoto,paymentError,setPaymentError,paymentSuccess,setPaymentSuccess]}>
-    <Router>
-
-      <Header></Header>
+    <userContext.Provider
+      value={[
+        user,
+        setUser,
+        servicePhoto,
+        setServicePhoto,
+        paymentError,
+        setPaymentError,
+        paymentSuccess,
+        setPaymentSuccess,
+      ]}
+    >
+      <Router>
+        <Header></Header>
 
         <Switch>
-
           <Route path="/home">
-              <Home></Home>
+            <Home></Home>
           </Route>
           <Route path="/review">
-              <About></About>
+            <About></About>
           </Route>
           <PrivateRoute path="/admin">
-              <Admin></Admin>
+            <Admin></Admin>
           </PrivateRoute>
           <Route path="/services">
-              <Projects></Projects>
+            <Projects></Projects>
           </Route>
           <Route path="/login">
-              <Login></Login>
+            <Login></Login>
           </Route>
           <Route path="/contact">
-              <Contact></Contact>
+            <Contact></Contact>
           </Route>
           <PrivateRoute path="/order/:idNumber">
-              <Order></Order>
-           </PrivateRoute>
-          <Route  path="/orderList">
-              <OrderList></OrderList>
+            <Order></Order>
+          </PrivateRoute>
+          <Route path="/orderList">
+            <OrderList></OrderList>
           </Route>
           <Route exact path="/">
-              <Home></Home>
+            <Home></Home>
           </Route>
-          <Route  path="*">
-              <NotFound></NotFound>
+          <Route path="*">
+            <NotFound></NotFound>
           </Route>
-          
-        
-        
-        
-
         </Switch>
-     
-    </Router>
+      </Router>
     </userContext.Provider>
-
   );
 }
 
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,12 +1,139 @@
-import React from 'react';
-import SideBar from '../SideBar/SideBar';
+// import SideBar from '../SideBar/SideBar';
+
+import { useContext, useState } from "react";
+import { userContext } from "../../App";
+import AddService from "../AddService/AddService";
+import MakeAddmin from "../MakeAddmin/MakeAddmin";
+import ManageService from "../ManageService/ManageService";
+import OrderList from "../OrderList/OrderList";
+import ReviewArea from "../ReviewArea/ReviewArea";
+import "./Admin.styles.css";
 
 const Admin = () => {
-    return (
-        <div className="container">
-            <SideBar></SideBar>
+  const [
+    user,
+    setUser,
+    servicePhoto,
+    setServicePhoto,
+    paymentError,
+    setPaymentError,
+    paymentSuccess,
+    setPaymentSuccess,
+  ] = useContext(userContext);
+  const [orderList, setOrderList] = useState(true);
+  const [review, setReview] = useState(false);
+  const [addService, setAddService] = useState(false);
+  const [makeAdmin, setMakeAdmin] = useState(false);
+  const [manageService, setManageService] = useState(false);
+
+  const found = servicePhoto.find((Element) => Element.email === user.email);
+
+  const handleOrder = () => {
+    setOrderList(true);
+    setAddService(false);
+    setMakeAdmin(false);
+    setManageService(false);
+    setReview(false);
+  };
+
+  const handleReview = () => {
+    setOrderList(false);
+    setAddService(false);
+    setMakeAdmin(false);
+    setManageService(false);
+    setReview(true);
+  };
+
+  const handleAddService = () => {
+    setOrderList(false);
+    setAddService(true);
+    setMakeAdmin(false);
+    setManageService(false);
+    setReview(false);
+  };
+
+  const handleMakeAdmin = () => {
+    setOrderList(false);
+    setAddService(false);
+    setMakeAdmin(true);
+    setManageService(false);
+    setReview(false);
+  };
+
+  const handleManageService = () => {
+    setOrderList(false);
+    setAddService(false);
+    setMakeAdmin(false);
+    setManageService(true);
+    setReview(false);
+  };
+  return (
+    <div className="container">
+      {/* <SideBar></SideBar> */}
+      <div className="row">
+        <div className="col-lg-4 col-sm-12 p-0">
+          <ul className="side-bar-list">
+            {!found ? (
+              <>
+                <li
+                  onClick={handleOrder}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  OrderList
+                </li>
+                <li
+                  onClick={handleReview}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  Review
+                </li>
+              </>
+            ) : (
+              <>
+                <li
+                  onClick={handleOrder}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  OrderList
+                </li>
+                <li
+                  onClick={handleReview}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  Review
+                </li>
+                <li
+                  onClick={handleAddService}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  Add Service
+                </li>
+                <li
+                  onClick={handleMakeAdmin}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  Make Admin
+                </li>
+                <li
+                  onClick={handleManageService}
+                  style={{ fontSize: "20px", color: "#FFC800" }}
+                >
+                  Manage Service
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-    );
+        <div className="col-lg-8 col-sm-12 p-0">
+          {orderList && <OrderList />}
+          {review && <ReviewArea />}
+          {addService && <AddService />}
+          {makeAdmin && <MakeAddmin />}
+          {manageService && <ManageService />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
