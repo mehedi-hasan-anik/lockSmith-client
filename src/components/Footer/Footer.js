f710410af82cfa9a73fb9fff1ea1b6fc4d198379
anik <anikm2283@gmail.com>
2022-06-18 17:26:18 +0600

    Added padding

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ import "./Footer.css";
 const Footer = () => {
   const About = [
     {
-      name: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      name: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy ",
       link: "/emergency",
     },
     // {name: "Check Up" , link: "/checkup"},
